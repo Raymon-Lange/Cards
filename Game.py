85b3c08b2fe237667bbb9daae7b15daba82eee7b
Raymon.Lange <raymon.lange@gmail.com>
2023-11-09 15:57:54 -0600

    added playable features like playing from hand to discard or field and goal to field.

--- Game.py
+++ Game.py
@@ -9,6 +9,10 @@ class Board:
         self.ready = False
         self.currentTurn = 0 # either zero or one 
 
+        self.displayedCard = []
+
+        self.activeCard = None
+
 
         #Game Objects
         self.playerOne = Player("One")
@@ -20,9 +24,8 @@ class Board:
         self.deck.shuffle()
 
         for i in range(1,15):
-            self.playerOne.goal.append(self.deck.deal())
-            self.playerTwo.goal.append(self.deck.deal())
-
+            self.play("start::", 0)
+            self.play("start::", 1)
 
         if self.deck.compare(self.playerOne.goal[0], self.playerTwo.goal[0]):
             self.currentTurn = 0
@@ -39,24 +42,125 @@ class Board:
     def reset(self):
         pass
     
-    def play(self,data):
-        self.field[0].append(self.deck.deal())
+    def checkValidMove(self, card):
+        pass
 
-    def endTurn(self):
-        if self.currentTurn == 0:
-            while len(self.playerOne.hand) != 4:
+    def play(self,data, playerId):
+
+        action, value, location = data.split(':')
+        print(data)
+
+        if action == "deal":
+            self.dealPlayer(playerId)
+        if action == "start":
+            self.startGame(playerId)
+        if action == "discard":
+            self.discard(value, location, playerId)
+        if action == "move":
+            self.move(value, location, playerId)
+
+
+    def startGame(self, playerId):
+        if playerId == 0:
+            self.playerOne.goal.append(self.deck.deal())
+            index = len(self.playerOne.goal) - 1
+            self.playerOne.goal[index].rect.x = 25
+            self.playerOne.goal[index].rect.y = 50
+        if playerId == 1:
+            self.playerTwo.goal.append(self.deck.deal())
+            index = len(self.playerTwo.goal) - 1
+            self.playerTwo.goal[index].rect.x = 25
+            self.playerTwo.goal[index].rect.y = 365
+
+    def dealPlayer(self, playerId):
+        if playerId == 0:
+            while len(self.playerOne.hand) != 5:
                 self.playerOne.hand.append(self.deck.deal())
-            self.currentTurn = 1
-        else:
-            while len(self.playerTwo.hand) != 4:
+                index = len(self.playerOne.hand) - 1
+                x = 140 + (81 * index)
+                y = 50
+                self.playerOne.hand[index].rect.x = x
+                self.playerOne.hand[index].rect.y = y
+        if playerId == 1:
+            while len(self.playerTwo.hand) != 5:
                 self.playerTwo.hand.append(self.deck.deal())
-            self.currentTurn = 0
-        
+                index = len(self.playerTwo.hand) - 1
+                x = 150 + (81 * index)
+                y = 365
+                self.playerTwo.hand[index].rect.x = x
+                self.playerTwo.hand[index].rect.y = y
+
+    def discard(self, value, location , playerId):
+        print(value)
+        if playerId == 0:
+            x = 80
+            y = 155
+            for card in self.playerOne.hand:
+                if str(card) == value:
+                    x += 91 * (int(location)+1)
+                    card.move(x,y)
+                    self.playerOne.discard[int(location)].append(card)
+                    self.playerOne.hand.remove(card)
+
+    def move(self, value, location, playerId):
+        if playerId == 0:
+            x = 50
+            y = 260
+            for card in self.playerOne.hand:
+                if str(card) == value:
+                    x += 91 * (int(location)+1)
+                    card.move(x,y)
+                    self.field[int(location)].append(card)
+                    self.playerOne.hand.remove(card)
+
+            if str(self.playerOne.goal[0]) == value:
+                card = self.playerOne.goal[0]
+                x += 91 * (int(location)+1)
+                card.move(x,y)
+                self.field[int(location)].append(card)
+                self.playerOne.goal.remove(card)
+
+
+    def playCard(self, card):
+        #STEP: First check to see if we discarded a card
+        discardY = 155
+        x = 80
+        for num, discardPile in enumerate(self.playerOne.discard):
+            x += 20 + 71 
+            if len(discardPile) == 0:
+                if pygame.Rect(x,discardY, 71,94).colliderect(card.rect):
+                    return "discard:"+ str(card)+":"+str(num)
+            else:
+                if discardPile[0].rect.colliderect(card.rect):
+                    return "discard:"+ str(card)+":"+str(num)
+                
+
+        #STEP: than check the field
+        fieldY = 260
+        x = 50
+
+        for num, field in enumerate(self.field):
+            x += 20+71
+            if len(field) == 0:
+                if pygame.Rect(x,fieldY,71,94).colliderect(card.rect):
+                    if card.rank == "King" or card.rank == "Ace":
+                        return "move:"+ str(card)+":"+str(num)
+            else:
+                if field[0].rect.colliderect(card.rect):
+                    cardCount = len(field)-1
+                    if self.deck.ranks.index(card.rank) - cardCount == 1:
+                        return "move:"+ str(card)+":"+str(num)
+
+
+        return None
+    
 
     def __str__(self):
-        return str(self.playerOne)
+        return str(self.playerOne) + "\n " + str(self.playerTwo)
     
     def draw(self, screen):
+
+        #STEP: Repaint the background
         screen.fill((0,100,0))
 
         ySpaceing = 105
@@ -65,25 +169,26 @@ class Board:
         x = 25
         y = 50
 
-        self.playerOne.goal[0].draw(screen, x,y,90)
+        self.playerOne.goal[0].draw(screen,90)
 
         x = 50
 
-        for card in range(0,4):
-            x += 20 + 71 
+        for card in range(0,5):
+            x += 12 + 71 
             if len(self.playerOne.hand) > card:
-                self.playerOne.hand[card].draw(screen, x,y)
-            else:
-                pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
+                self.playerOne.hand[card].draw(screen)
+            #else:
+            #    pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
 
-        x = 50
+        x = 80
         y += ySpaceing
 
         for card in self.playerOne.discard:
             x += 20 + 71 
             if len(card) == 0:
                 pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
-
+            else:
+                card[len(card)-1].draw(screen)
 
         # Draw the field
         x = 50
@@ -94,33 +199,7 @@ class Board:
             if len(card) == 0:
                 pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
             else:
-                card[len(card)-1].draw(screen,x,y)
-
-        #STEP: Draw Player 2 
-        x = 50
-        y += ySpaceing
-
-        for card in self.playerTwo.discard:
-            x += 20 + 71 
-            if len(card) == 0:
-                pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
-
-
-        x = 25
-        y += ySpaceing
-        
-        self.playerTwo.goal[0].draw(screen, x,y,90)
-
-        x = 50
-
-        for card in range(0,4):
-            x += 20 + 71 
-            if len(self.playerTwo.hand) > card:
-                self.playerTwo.hand[card].draw(screen, x,y)
-            else:
-                pygame.draw.rect(screen, (0, 255, 0), (x,y,71,94), 2)
-        
-
+                card[len(card)-1].draw(screen)
 
 class Player:
     def __init__(self, name):
@@ -142,7 +221,7 @@ class Deck:
     def __init__(self, decks =1):
         self.cards = []
         self.suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-        self.ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+        self.ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
         for deck in range(1,decks):
             for suit in self.suits:
@@ -170,10 +249,15 @@ class Card(pygame.sprite.Sprite):
         self.rank = rank
 
         self.image = pygame.image.load(join("assets", suit, suit+rank+".png"))
+        self.rect = self.image.get_rect()
   
     def __str__(self):
         return f"{self.rank} of {self.suit}"
     
-    def draw(self,screen, x, y, rotate =0):
+    def move(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+    
+    def draw(self,screen, rotate =0):
         rotated_image = pygame.transform.rotate(self.image, rotate)
-        screen.blit(rotated_image, (x,y))
+        screen.blit(rotated_image, self.rect)
